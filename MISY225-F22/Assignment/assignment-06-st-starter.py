8d0bf0615bf8d41cb1f93fb53996135abf9294f3
Derekwang2002 <singandsing@icloud.com>
2022-10-27 00:31:37 +0800

    add streamlit

---
+++ MISY225-F22/Assignment/assignment-06-st-starter.py
@@ -0,0 +1,32 @@
+# import packages
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# show the title
+st.title('EDA for TITANIC dataset')
+
+# read csv and show the dataframe
+ttnic_train = pd.read_csv('train.csv')
+st.write(ttnic_train)
+
+# create a figure with three subplots, size should be (15, 5)
+fig, ax = plt.subplots(1, 3, figsize=(15,5))
+
+# show the box plot for ticket price with different classes
+# you need to set the x labels and y labels
+i = 0
+
+for class_name, class_df in ttnic_train.groupby('Pclass'):
+
+    class_df.Fare.plot.box(ax=ax[i])
+    ax[i].set_xlabel(f'Pclass = {class_name}')
+    ax[i].set_ylabel('Fare')
+
+    i += 1
+
+
+
+
+# a sample diagram is shown below
+
