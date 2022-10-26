cefe6e25026b498293baf7ef8daae6f24e2d9967
Derekwang2002 <singandsing@icloud.com>
2022-10-27 00:40:18 +0800

    updated .py

--- MISY225-F22/Assignment/assignment-06-st-starter.py
+++ MISY225-F22/Assignment/assignment-06-st-starter.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 # show the title
-st.title('EDA for TITANIC dataset')
-
+st.title('EDA for TITANIC dataset - by Wang Xing\'en')
+st.header('Below is a datafraom')
 # read csv and show the dataframe
 ttnic_train = pd.read_csv('train.csv')
 st.write(ttnic_train)
 
 # create a figure with three subplots, size should be (15, 5)
+st.header('Below is box plots for ticket price with different classes')
 fig, ax = plt.subplots(1, 3, figsize=(15,5))
 
 # show the box plot for ticket price with different classes
@@ -25,8 +26,7 @@ for class_name, class_df in ttnic_train.groupby('Pclass'):
 
     i += 1
 
+st.pyplot(fig)
 
 
-
-# a sample diagram is shown below
 
